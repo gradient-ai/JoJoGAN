2cb204a5d97ded7238bccda1383dbef9b2f02a94
jameshskelton <jameshskelton@gmail.com>
2022-01-10 21:11:35 -0500

    added scripts

---
+++ Run_Stylizer.py
@@ -0,0 +1,169 @@
+import torch
+torch.backends.cudnn.benchmark = True
+from torchvision import transforms, utils
+from util import *
+from PIL import Image
+import math
+import random
+import os
+
+import numpy as np
+from torch import nn, autograd, optim
+from torch.nn import functional as F
+from tqdm import tqdm
+import lpips
+import wandb
+from JoJoGAN.model import *
+from JoJoGAN.e4e_projection import projection as e4e_projection
+from copy import deepcopy
+device = 'cuda' #@param ['cuda', 'cpu']
+
+latent_dim = 512
+
+# Load original generator
+original_generator = Generator(1024, latent_dim, 8, 2).to(device)
+ckpt = torch.load('stylegan2-ffhq-config-f.pt')
+original_generator.load_state_dict(ckpt["g_ema"], strict=False)
+mean_latent = original_generator.mean_latent(10000)
+
+# to be finetuned generator
+generator = deepcopy(original_generator)
+
+transform = transforms.Compose(
+    [
+        transforms.Resize((1024, 1024)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    ]
+)
+#@title Finetune StyleGAN
+#@markdown alpha controls the strength of the style
+alpha =  1.0 #@param {type:"slider", min:0, max:1, step:0.1}
+alpha = 1-alpha
+
+#@markdown Tries to preserve color of original image by limiting family of allowable transformations. Set to false if you want to transfer color from reference image. This also leads to heavier stylization
+preserve_color = True #@param{type:"boolean"}
+#@markdown Number of finetuning steps. Different style reference may require different iterations. Try 200~500 iterations.
+num_iter = 200 #@param {type:"number"}
+
+filename = 'IMG_3075.jpeg' #@param {type:"string"}
+filepath = f'test_input/{filename}'
+
+# uploaded = files.upload()
+# filepath = list(uploaded.keys())[0]
+name = strip_path_extension(filepath)+'.pt'
+
+# aligns and crops face
+aligned_face = align_face(filepath)
+
+# my_w = restyle_projection(aligned_face, name, device, n_iters=1).unsqueeze(0)
+my_w = e4e_projection(aligned_face, name, device).unsqueeze(0)
+
+#@markdown Upload your own style images into the style_images folder and type it into the field in the following format without the directory name. Upload multiple style images to do multi-shot image translation
+from os import listdir
+from os.path import isfile, join
+names = [f for f in listdir('style_images/AF_dataset/Vincent_van_Gogh/') if isfile(join('style_images/AF_dataset/Vincent_van_Gogh/', f))]
+
+targets = []
+latents = []
+
+for name in names:
+    style_path = os.path.join('style_images/AF_dataset/Vincent_van_Gogh/', name)
+    assert os.path.exists(style_path), f"{style_path} does not exist!"
+
+    name = strip_path_extension(name)
+
+    # crop and align the face
+    style_aligned_path = os.path.join('style_images_aligned', f'{name}.png')
+    if not os.path.exists(style_aligned_path):
+        style_aligned = align_face(style_path)
+        style_aligned.save(style_aligned_path)
+    else:
+        style_aligned = Image.open(style_aligned_path).convert('RGB')
+
+    # GAN invert
+    style_code_path = os.path.join('inversion_codes', f'{name}.pt')
+    if not os.path.exists(style_code_path):
+        latent = e4e_projection(style_aligned, style_code_path, device)
+    else:
+        latent = torch.load(style_code_path)['latent']
+
+    targets.append(transform(style_aligned).to(device))
+    latents.append(latent.to(device))
+
+targets = torch.stack(targets, 0)
+latents = torch.stack(latents, 0)
+
+target_im = utils.make_grid(targets, normalize=True, range=(-1, 1))
+plt.rcParams['figure.dpi'] = 150
+pretrained = 'disney' #@param ['art', 'arcane_multi', 'supergirl', 'arcane_jinx', 'arcane_caitlyn', 'jojo_yasuho', 'jojo', 'disney']
+#@markdown Preserve color tries to preserve color of original image by limiting family of allowable transformations. Otherwise, the stylized image will inherit the colors of the reference images, leading to heavier stylizations.
+preserve_color = False #@param{type:"boolean"}
+
+ckpt = torch.load('disney.pt')
+generator.load_state_dict(ckpt["g"], strict=False)
+
+#@title Generate results
+n_sample =  5#@param {type:"number"}
+seed = 3000 #@param {type:"number"}
+
+torch.manual_seed(seed)
+with torch.no_grad():
+    generator.eval()
+    z = torch.randn(n_sample, latent_dim, device=device)
+
+    original_sample = original_generator([z], truncation=0.7, truncation_latent=mean_latent)
+    sample = generator([z], truncation=0.7, truncation_latent=mean_latent)
+
+    original_my_sample = original_generator(my_w, input_is_latent=True)
+    my_sample = generator(my_w, input_is_latent=True)
+
+# display reference images
+if pretrained == 'arcane_multi':
+    style_path = f'style_images_aligned/arcane_jinx.png'
+else:   
+    style_path = f'style_images_aligned/{pretrained}.png'
+style_image = transform(Image.open(style_path)).unsqueeze(0).to(device)
+face = transform(aligned_face).unsqueeze(0).to(device)
+
+my_output = torch.cat([style_image, face, my_sample], 0)
+
+output = torch.cat([original_sample, sample], 0)
+
+
+
+lpips_fn = lpips.LPIPS(net='vgg').to(device)
+
+# reset generator
+del generator
+generator = deepcopy(original_generator)
+
+g_optim = optim.Adam(generator.parameters(), lr=2e-3, betas=(0, 0.99))
+
+# Which layers to swap for generating a family of plausible real images -> fake image
+# def run():
+if preserve_color:
+    id_swap = [7,9,11,15,16,17]
+else:
+    id_swap = list(range(7, generator.n_latent))
+
+for idx in tqdm(range(num_iter)):
+    if preserve_color:
+        random_alpha = 0
+    else:
+        random_alpha = np.random.uniform(alpha, 1)
+    mean_w = generator.get_latent(torch.randn([latents.size(0), latent_dim]).to(device)).unsqueeze(1).repeat(1, generator.n_latent, 1)
+    in_latent = latents.clone()
+    in_latent[:, id_swap] = alpha*latents[:, id_swap] + (1-alpha)*mean_w[:, id_swap]
+
+    img = generator(in_latent, input_is_latent=True)
+    loss = lpips_fn(F.interpolate(img, size=(256,256), mode='area'), F.interpolate(targets, size=(256,256), mode='area')).mean()
+    
+
+    g_optim.zero_grad()
+    loss.backward()
+    g_optim.step()
+
+# if __name__ == "__main__":
+#     run()
+   
